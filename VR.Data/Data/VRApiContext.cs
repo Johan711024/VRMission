51e7c05ccf5ba3d0aa2da4bef525be2dea6ac7bf
Johan Svensson <hyperjohan@hotmail.com>
2023-01-11 10:43:22 +0100

    first

--- VR.Data/Data/VRApiContext.cs
+++ VR.Data/Data/VRApiContext.cs
@@ -14,6 +14,7 @@ namespace VR.Data.Data
         {
         }
 
+
         public DbSet<VRMission.Person> Person { get; set; } = default!;
 
         public DbSet<VRMission.Mission> Mission { get; set; } = default!;
